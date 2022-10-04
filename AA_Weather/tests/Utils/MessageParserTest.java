66180d8dd629c951d7551e0faccaad141f1f71ba
Francisco Wendeburg <wendeburgf@gmail.com>
2022-10-04 09:47:22 +0200

    tests setup

---
+++ AA_Weather/tests/Utils/MessageParserTest.java
@@ -0,0 +1,14 @@
+package Utils;
+
+import org.junit.Test;
+
+public class MessageParserTest {
+    private final MessageParser parser = new MessageParser();
+
+    @Test
+    public void getLRCTest1() {
+    }
+
+    void getLRCTest2() {
+    }
+}
