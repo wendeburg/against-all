d865ecf37b22b57f7a526bcf4994487d05c8be54
aed6-ua <aed6@alu.ua.es>
2022-11-10 18:28:32 +0100

    arreglado loop de keepalive infinito

--- AA_Player/AA_Player.py
+++ AA_Player/AA_Player.py
@@ -123,6 +123,8 @@ class Player:
             if self.move is None:
                 time.sleep(1)
                 self.producer.send("PLAYERMOVEMENTS", {self.token: "KA"})
+            if self.muerto:
+                break
     
     def start_read(self):
         self.receive_message()
@@ -132,10 +134,15 @@ class Player:
             message_count = 0
             for message in self._consumer:
                 message = message.value
+                if self.token not in message["jugadores"]:
+                    print(bcolors.WARNING + "MUELTO" + bcolors.ENDC)
+                    break
+                jugador = message["jugadores"][self.token]
+                print("Nivel:", jugador["nivel"])
                 map = message['mapa']
                 cities = list(message['ciudades'].keys())
                 os.system("cls||clear")
-                print('Message', message_count, ':')
+                #print('Message', message_count, ':')
                 string_mapa=""
                 string_mapa+=(cities[0]+': '+str(message['ciudades'][cities[0]])+ '             '+cities[1]+': '+ str(message['ciudades'][cities[1]])+"\n")
                 string_mapa+=('---------------------|---------------------'+"\n")
@@ -252,6 +259,7 @@ class Player:
             send_kafka.start()
             receive_kafka.join()
             print("Game end")
+            self.muerto=True
         except Exception as exc:
             print("Ha ocurrido un error en la conexión:", exc)
 
@@ -282,6 +290,7 @@ if (len(sys.argv)==7):
             player.join_game()
             time.sleep(2)
         if opcion==4:
+            os._exit()
             break
 else:
     print("Oops!. Something went bad. I need following args: <Registry_Server_IP> <Registry_Server_Port> <Auth_Server_IP> <Auth_Server_Port> <Bootstrap_Server_IP> <Bootstrap_Server_Port>")
