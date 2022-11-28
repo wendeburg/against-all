4fdd8ac208e40c6cc43a8e267943cec2af20c1a9
Francisco Wendeburg <wendeburgf@gmail.com>
2022-11-28 09:22:31 +0100

    ahora genera .pems

--- creador_keystore.py
+++ creador_keystore.py
@@ -10,6 +10,7 @@ ks_pass = sys.argv[4]
 ca_cert_path = sys.argv[5]
 ca_key_path = sys.argv[6]
 srl_file_path = sys.argv[7]
+cvt_to_pem = sys.argv[8]
 
 dst_path = f"./{module_name}_secrets/"
 
@@ -31,5 +32,13 @@ for i in range(ks_num_start, ks_num_end):
     os.system(f"keytool -keystore {dst_path}{module_name}.{i}.keystore.jks -alias CARoot -import -file {ca_cert_path} -storepass {ks_pass} -keypass {ks_pass}")
 
     os.system(f"keytool -keystore {dst_path}{module_name}.{i}.keystore.jks -alias {module_name}-{i} -import -file {dst_path}{module_name}-{i}-signed.csr -storepass {ks_pass} -keypass {ks_pass}")
+
+    if (cvt_to_pem == "y"):
+        os.system(f"keytool -exportcert -alias {module_name}-{i} -keystore {dst_path}{module_name}.{i}.keystore.jks -rfc -file {dst_path}{module_name}.{i}.certificate.pem -storepass {ks_pass}")
+
+        os.system(f"keytool -v -importkeystore -srckeystore {dst_path}{module_name}.{i}.keystore.jks -srcalias {module_name}-{i} -destkeystore {dst_path}{module_name}.{i}.cert_and_key.p12 -deststoretype PKCS12 -storepass {ks_pass} -srcstorepass {ks_pass}")
+        os.system(f"openssl pkcs12 -in {dst_path}{module_name}.{i}.cert_and_key.p12 -nodes -nocerts -out {dst_path}{module_name}.{i}.key.pem -passin pass:{ks_pass}")
+
+        os.system(f"keytool -exportcert -alias caroot -keystore {dst_path}{module_name}.{i}.keystore.jks -rfc -file {dst_path}{module_name}.{i}.CARoot.pem -storepass {ks_pass}")
 
 os.system(f"echo {ks_pass} > {dst_path}{module_name}_creds")
